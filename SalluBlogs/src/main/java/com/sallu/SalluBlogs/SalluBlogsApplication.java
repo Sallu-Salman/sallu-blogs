b747b45b2a9adced994517e4a16c17f994a0bc39
Salman <sallumech2023@gmail.com>
2023-06-18 18:30:35 +0530

    Project setup

---
+++ SalluBlogs/src/main/java/com/sallu/SalluBlogs/SalluBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.sallu.SalluBlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalluBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalluBlogsApplication.class, args);
+	}
+
+}
---
+++ SalluBlogs/src/test/java/com/sallu/SalluBlogs/SalluBlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sallu.SalluBlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalluBlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
